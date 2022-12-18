b48195e45af1aa15d43999c6a1f369a06b30467a
Amin Kamal <aminkamal355@gmail.com>
2022-12-18 21:35:22 +0800

    tugas 16
    tugas 16

---
+++ tugas16.js
@@ -0,0 +1,8 @@
+function mix(){
+    min = Math.min(45,100,99,3,2,4,1,2,3,19,22);
+    console.log("Nilai Minimal =", min)
+    max = Math.max(45,100,99,3,2,4,1,2,3,19,22);
+    console.log("Nilai Minimal =", max)
+}
+
+mix()
